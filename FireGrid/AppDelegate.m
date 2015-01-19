e1d2b9df3e7990f18242307bf887f62835931996
Kunal Batra <kunal.batra@sendgrid.com>
2015-01-18 22:27:17 -0500

    Changed observeEventType to FEventTypeChildAdded

--- FireGrid/AppDelegate.m
+++ FireGrid/AppDelegate.m
@@ -3,7 +3,7 @@
 //  FireGrid
 //
 //  Created by Kunal Batra on 12/30/14.
-//  Copyright (c) 2014 SendGrid. All rights reserved.
+//  Copyright (c) 2014 SendGrid Inc. All rights reserved.
 //
 
 #import "AppDelegate.h"
@@ -22,51 +22,41 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
    
     
-    // Get a reference to our posts
-    Firebase *ref = [[Firebase alloc] initWithUrl: @"your_firebase_url"];
     
-    
-    //Initialize the Status bar app with Image and Tool tip
+    //Initialize the Status bar app with SendGrid Image and Tool tip
     self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
     _statusItem.image = [NSImage imageNamed:@"sendgrid.png"];
     [_statusItem setToolTip:@"SendGrid Event feed"];
     
-    
-    self.theMenu = [[NSMenu alloc] initWithTitle:@"First"];
+    //Initialize the menu
+    self.theMenu = [NSMenu alloc];
     [_theMenu setAutoenablesItems:NO];
-    
     NSMenuItem *tItem = nil;
     [_theMenu addItemWithTitle:@"SendGrid Email Activity:" action:nil keyEquivalent:@""];
     [_theMenu addItemWithTitle:@"" action:nil keyEquivalent:@""];
     
     
+    //Hook up menu with Status bar app
+    [tItem setKeyEquivalentModifierMask:NSCommandKeyMask];
+    [_statusItem setMenu:_theMenu];
     
-    [ref observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
-        //for every child in the snapshod we create a menu item
-        for (FDataSnapshot *child in snapshot.children) {
-          
-            NSDate *tstamp2 = [[NSDate alloc] initWithTimeIntervalSince1970:[snapshot.value[child.key][@"timestamp"] intValue]];
-            NSString *menuitem = [NSString stringWithFormat:@" %@ %@ed at %@",snapshot.value[child.key][@"email"], snapshot.value[child.key][@"event"], tstamp2 ];
-            [_theMenu addItemWithTitle:menuitem action:nil keyEquivalent:@""];
-            [_theMenu addItem:[NSMenuItem separatorItem]];
-        }
+    //Add a new menu item for every child added.
+    Firebase *ref2 = [[Firebase alloc] initWithUrl: @"Your_Firebasę_URL"];
+    [[ref2 queryLimitedToLast:20] observeEventType:FEventTypeChildAdded withBlock:^(FDataSnapshot *snapshot) {
         
+        //Create string
+        NSDate *tstamp2 = [[NSDate alloc] initWithTimeIntervalSince1970:[snapshot.value[@"timestamp"] intValue]];
+        NSString *menuitem = [NSString stringWithFormat:@" %@ %@ at %@",snapshot.value[@"email"], snapshot.value[@"event"], tstamp2 ];
+        
+        //Create menu item from string
+        [_theMenu addItemWithTitle:menuitem action:nil keyEquivalent:@""];
+        [_theMenu addItem:[NSMenuItem separatorItem]];
         
     } withCancelBlock:^(NSError *error) {
         NSLog(@"%@", error.description);
     }];
-    
-    
-  
-    [tItem setKeyEquivalentModifierMask:NSCommandKeyMask];
-    
-    
-
-    [_statusItem setMenu:_theMenu];
-
 
     
-    // Insert code here to initialize your application
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
